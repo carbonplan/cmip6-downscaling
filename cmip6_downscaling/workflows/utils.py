114883934ea3d22dfbd1d047f20d585fd4625dab
Joe Hamman <joe@carbonplan.org>
2020-12-10 19:20:34 +0000

    prototype new parallel (delayed-based) terraclimate cmip script

--- cmip6_downscaling/workflows/utils.py
+++ cmip6_downscaling/workflows/utils.py
@@ -3,12 +3,16 @@ import os
 import zarr
 
 
-def get_store(prefix):
+def get_store(prefix, account_key=None):
     ''' helper function to create a zarr store'''
+
+    if account_key is None:
+        account_key = os.environ.get('BLOB_ACCOUNT_KEY', None)
+
     store = zarr.storage.ABSStore(
         'carbonplan-downscaling',
         prefix=prefix,
         account_name="carbonplan",
-        account_key=os.environ["BLOB_ACCOUNT_KEY"],
+        account_key=account_key,
     )
     return store
