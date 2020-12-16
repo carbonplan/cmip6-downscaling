c24908a7b05f206b374d6c6cd47caba81fcd4c9b
Oriana Chegwidden <orianac@uw.edu>
2020-12-16 06:17:44 +0000

    Adding a couple more elements to plotting and metrics

--- cmip6_downscaling/analysis/metrics.py
+++ cmip6_downscaling/analysis/metrics.py
@@ -1,3 +1,7 @@
+import numpy as np
+from esda.moran import Moran
+
+
 def weighted_mean(ds, *args, **kwargs):
     weights = ds.time.dt.days_in_month
     return ds.weighted(weights).mean(dim='time')
@@ -16,6 +20,19 @@ def seasonal_cycle_std(obj):
     return obj.mean(('x', 'y'))
 
 
+def moransI(obj, weights=None):
+    """
+    Inputs:
+    obj: xarray object for which you want the autocorrelation
+    weights: same shape as obj, but with weights which will be applied to
+    obj to scale the autocorrelation. Simplest could be all ones.
+    """
+    if not weights:
+        weights = np.ones_like(obj.values)
+
+    return Moran(obj.values, weights)
+
+
 # def extreme_scaling(obj, factors=[1, 2, 4, 8, 16], q=0.02):
 
 #     for factor in factors:
@@ -25,11 +42,18 @@ def seasonal_cycle_std(obj):
 
 
 def calc(obj, compute=False):
+    """
+    This function takes an object and then calculates a
+    series of metrics for that object. It returns it
+    as a dictionary object of xarray objects. If the compute flag
+    is turned on these objects will be in memory. Otherwise the
+    computations will be lazy.
+    """
 
     metrics = {}
     if compute:
-        metrics['time_mean'] = time_mean(obj).compute()
-        metrics['seasonal_cycle_mean'] = seasonal_cycle_mean(obj).compute()
+        metrics['time_mean'] = time_mean(obj).load()
+        metrics['seasonal_cycle_mean'] = seasonal_cycle_mean(obj).load()
     else:
         metrics['time_mean'] = time_mean(obj)
         metrics['seasonal_cycle_mean'] = seasonal_cycle_mean(obj)
--- cmip6_downscaling/analysis/plot.py
+++ cmip6_downscaling/analysis/plot.py
@@ -18,3 +18,21 @@ def plot_time_mean(ds1, ds2, diff=True, limits_dict=None):
         (ds2 - ds1)[var].plot(ax=axarr[i, 2])
 
     return fig, axarr
+
+
+def plot_seasonal_mean(ds1, ds2, limits_dict=None):
+
+    vars_to_plot = list(ds1.data_vars.keys())
+
+    fig, axarr = plt.subplots(nrows=len(vars_to_plot))
+
+    for i, var in enumerate(vars_to_plot):
+        if var in list(limits_dict.keys()):
+            vmin = limits_dict[var]['vmin']
+            vmax = limits_dict[var]['vmax']
+        else:
+            vmin, vmax = None, None
+        ds2[var].plot(ax=axarr[i], vmin=vmin, vmax=vmax)
+        ds2[var].plot(ax=axarr[i], vmin=vmin, vmax=vmax)
+
+    return fig, axarr
