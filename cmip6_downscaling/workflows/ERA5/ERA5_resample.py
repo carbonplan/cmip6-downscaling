e508e362d7875794b6642b48744690d19548b575
Raphael Hagen <norlandrhagen@gmail.com>
2021-10-28 14:55:54 -0700

    ERA5_resample edits as PR (#27)
    * ERA5_resample edits as PR
    
    * changed chunk from time to space
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * incorporated comments from Ori. Added test for cat retrieval, moved get_store func to share.py and fixed linting issue in workflow.ipynb
    
    * added prefect to requirments.txt to satisfy pyest
    
    * r2 of Ori's feedback
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- cmip6_downscaling/workflows/ERA5/ERA5_resample.py
+++ cmip6_downscaling/workflows/ERA5/ERA5_resample.py
@@ -1,46 +1,38 @@
+"""Script for resampling and reformatting existing ERA5 zarr stores into a single, daily zarr store with matching variable names to CMIP6 GCM data."""
+
 import os
 
 import fsspec
 import intake
 import xarray as xr
-import zarr
 from prefect import Flow, task
 from prefect.run_configs import KubernetesRun
 from prefect.storage import Azure
+
+from cmip6_downscaling.workflows.share import get_store
 
 connection_string = os.environ.get("AZURE_STORAGE_CONNECTION_STRING")
 account_key = os.environ.get("account_key")
 
 
-def get_store(bucket, prefix, account_key=None):
-    """helper function to create a zarr store"""
-    if account_key is None:
-        account_key = os.environ.get('AccountKey', None)
-
-    store = zarr.storage.ABSStore(
-        bucket, prefix=prefix, account_name="cmip6downscaling", account_key=account_key
-    )
-    return store
-
-
-def get_zstore_list():
-    account_key = os.environ.get('AccountKey', None)
+def get_ERA5_zstore_list():
+    account_key = os.environ.get("AccountKey", None)
     col = intake.open_esm_datastore(
         "https://cmip6downscaling.blob.core.windows.net/cmip6/ERA5_catalog.json"
     )
-    zstores = [zstore.split('az://cmip6/')[1] for zstore in col.df.zstore.to_list()]
+    zstores = [zstore.split("az://cmip6/")[1] for zstore in col.df.zstore.to_list()]
     store_list = [
-        get_store(bucket='cmip6', prefix=prefix, account_key=account_key) for prefix in zstores
+        get_store(bucket="cmip6", prefix=prefix, account_key=account_key) for prefix in zstores
     ]
     return store_list
 
 
 def get_var_name_dict():
     var_name_dict = {
-        'eastward_wind_at_10_metres': 'uas',
-        'northward_wind_at_10_metres': 'vas',
-        'eastward_wind_at_100_metres': 'ua100m',
-        'northward_wind_at_100_metres': 'va100m',
+        "eastward_wind_at_10_metres": "uas",
+        "northward_wind_at_10_metres": "vas",
+        "eastward_wind_at_100_metres": "ua100m",
+        "northward_wind_at_100_metres": "va100m",
         "dew_point_temperature_at_2_metres": "tdps",
         "air_temperature_at_2_metres": "tas",
         "air_temperature_at_2_metres_1hour_Maximum": "tasmax",
@@ -61,61 +53,61 @@ def map_tgt(tgt, connection_string):
 
 
 def get_storage_chunks(chunk_direction):
-    if chunk_direction.lower() == 'time':
+
+    if chunk_direction.lower() == "time":
         # mdf.isel(time=slice(0,2)).nbytes/1e6 = ~102 MB. So every two days of full slices is 100MB
         target_chunks = {"lat": 721, "lon": 1440, "time": 2}
-    elif chunk_direction.lower() == 'space':
-        target_chunks = {"lat": 10, "lon": 10}
+    elif chunk_direction.lower() == "space":
+        # time_mult = 368184 (time_mult is # of time entries) -- (ds.isel(time=0,lat=slice(0,2),lon=slice(0,2)).nbytes * time_mult)/1e6 = ~88MB
+        target_chunks = {"lat": 2, "lon": 2, "time": -1}
     return target_chunks
 
 
 def write_zarr_store(ds):
     mapped_tgt = map_tgt("az://cmip6/ERA5/ERA5_resample_chunked_time/", connection_string)
-    ds.to_zarr(mapped_tgt, mode='w', consolidated=True)
+    ds.to_zarr(mapped_tgt, mode="w", consolidated=True)
 
 
 @task()
-def downsample_and_combine():
-
+def downsample_and_combine(chunking_method):
     # grab zstore list and variable rename dictionary
     var_name_dict = get_var_name_dict()
-    store_list = get_zstore_list()
+    store_list = get_ERA5_zstore_list()
 
     # Open all zarr stores for ERA5
-    ds = xr.open_mfdataset(store_list, engine='zarr', concat_dim='time')
+    ds = xr.open_mfdataset(store_list, engine="zarr", concat_dim="time")
 
     # drop time1 bounds
-    ds = ds.drop('time1_bounds')
+    ds = ds.drop("time1_bounds")
 
     # rename vars to match CMIP6 conventions
     ds = ds.rename(var_name_dict)
 
-    # temp vals for subsetting in resample
-    tasmaxmin = [
-        var_name_dict.pop(k, None)
-        for k in [
-            'air_temperature_at_2_metres_1hour_Maximum',
-            'air_temperature_at_2_metres_1hour_Minimum',
-        ]
-    ]
+    # mean vars
+    ds["uas"] = ds["uas"].resample(time="D").mean(keep_attrs=True)
+    ds["vas"] = ds["vas"].resample(time="D").mean(keep_attrs=True)
+    ds["ua100m"] = ds["ua100m"].resample(time="D").mean(keep_attrs=True)
+    ds["va100m"] = ds["va100m"].resample(time="D").mean(keep_attrs=True)
+    ds["tdps"] = ds["tdps"].resample(time="D").mean(keep_attrs=True)
+    ds["tas"] = ds["tas"].resample(time="D").mean(keep_attrs=True)
+    ds["psl"] = ds["psl"].resample(time="D").mean(keep_attrs=True)
+    ds["tos"] = ds["tos"].resample(time="D").mean(keep_attrs=True)
+    ds["ps"] = ds["ps"].resample(time="D").mean(keep_attrs=True)
 
-    # split dataset for differant resample strategies
-    ds_max = ds[[tasmaxmin[0]]].resample(time='D').max(keep_attrs=True)
-    ds_min = ds[[tasmaxmin[1]]].resample(time='D').min(keep_attrs=True)
-    ds_mean = ds[list(var_name_dict.values())].resample(time='D').mean(keep_attrs=True)
+    # summed vars
+    ds["rsds"] = ds["rsds"].resample(time="D").sum(keep_attrs=True) / 86400.0
+    ds["pr"] = ds["pr"].resample(time="D").sum(keep_attrs=True) / 86400.0 * 1000.0
 
-    # merge resampled datasets
-    mdf = xr.merge([ds_max, ds_min, ds_mean])
-
-    # convert integral_wrt_time_of_surface_direct_downwelling_shortwave_flux_in_air_1hour_Accumulation to match flux units in cmip6
-    mdf['rsds'] = mdf['rsds'] / 86400.0
+    # min/max vars
+    ds["tasmax"] = ds["tasmax"].resample(time="D").max(keep_attrs=True)
+    ds["tasmin"] = ds["tasmin"].resample(time="D").min(keep_attrs=True)
 
     # write CRS
-    mdf = mdf.rio.write_crs('EPSG:4326')
+    ds = ds.rio.write_crs("EPSG:4326")
 
     # write data as consolidated zarr store
-    storage_chunks = get_storage_chunks('time')
-    write_zarr_store(mdf, storage_chunks=storage_chunks)
+    storage_chunks = get_storage_chunks(chunking_method)
+    write_zarr_store(ds, storage_chunks=storage_chunks)
 
 
 run_config = KubernetesRun(
@@ -123,9 +115,14 @@ run_config = KubernetesRun(
     memory_request="3Gi",
     image="gcr.io/carbonplan/hub-notebook:7252fc3",
     labels=["az-eu-west"],
+    env={"EXTRA_PIP_PACKAGES": "git+git://github.com/carbonplan/cmip6-downscaling"},
 )
 storage = Azure("prefect")
 
-
-with Flow(name="Resample_ERA5_chunked_time", storage=storage, run_config=run_config) as flow:
-    downsample_and_combine()
+chunking_method = "space"
+with Flow(
+    name=f"Resample_ERA5_chunked_{chunking_method}",
+    storage=storage,
+    run_config=run_config,
+) as flow:
+    downsample_and_combine(chunking_method)
