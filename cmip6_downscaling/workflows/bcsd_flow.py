fbd6c9dde7afb8ccd1b6a4430bccd43a6af7d1ba
Oriana Chegwidden <orianachegwidden@Orianas-MacBook-Pro.local>
2021-11-10 10:47:51 -0800

    updates to workflow and underlying utilities. this pulls out tools that will be shared by other downscaling methods into shared modules (e.g. observations, cmip, utils). the flow is still in progress, so going to add the tools to main but leave the bcsd_flow on this branch in an open PR

--- cmip6_downscaling/workflows/bcsd_flow.py
+++ cmip6_downscaling/workflows/bcsd_flow.py
@@ -9,56 +9,14 @@ import xesmf as xe
 import zarr
 from prefect import Flow, Parameter, task
 from skdownscale.pointwise_models import BcAbsolute, PointWiseDownscaler
+from ..data.observations import load_obs, get_coarse_obs, get_spatial_anomolies
+from ..data.cmip import load_cmip_dictionary, gcm_munge, convert_to_360
+from ..utils import calc_auspicious_chunks_dict
 
-from cmip6_downscaling.workflows.utils import rechunk_dataset
+from cmip6_downscaling.workflows.utils import rechunk_dataset, convert_to_360
 
-# https://rechunker.readthedocs.io/en/latest/
-
-# Rechunker Example code:
-# import xarray as xr
-# xr.set_options(display_style='text')
-# import zarr
-# import dask.array as dsa
-# import fsspec
-# from rechunker import rechunk
-# import os
-
-
-# path = 'az://cmip6/ERA5/1979/01/air_pressure_at_mean_sea_level.zarr'
-# coarse_obs_store = fsspec.get_mapper(path, connection_string=connection_string)
-# ds = xr.open_zarr(coarse_obs_store, consolidated=True) # load the obs that fits the grid of the experiment
-# target_chunks_dict = {'time': 1, 'lat': 721, 'lon': 1440}
-# # target_chunks = (1,721,1440)
-# max_mem = '1GB'
-
-
-# store_tmp = fsspec.get_mapper('az://cmip6/rechunker_temp5.zarr', connection_string=connection_string)
-# store_tgt = fsspec.get_mapper('az://cmip6/rechunker_target5.zarr', connection_string=connection_string)
-
-# r = rechunk(ds, target_chunks_dict, max_mem,
-#                       store_tgt, temp_store=store_tmp)
-# result = r.execute()
-
-
-# store_tgt = fsspec.get_mapper('az://cmip6/rechunker_target2.zarr', connection_string=connection_string)
-
-# xdf = xr.open_zarr( store_tgt)
-
-
-# specify your run parameters by reading in a text config file?
-# potential test: ensure that the run_id in the config file is
-# the same as the name of the config file? to make sure that
-# you've created a new config file for a new run?
 chunks = {"lat": 10, "lon": 10, "time": -1}
 connection_string = os.environ.get("AZURE_STORAGE_CONNECTION_STRING")
-
-
-def convert_to_360(lon):
-    if lon > 0:
-        return lon
-    elif lon < 0:
-        return 360 + lon
-
 
 test_specs = {
     "domain": {
@@ -103,234 +61,6 @@ variable_name_dict = {
     "pr": "precipitation_amount_1hour_Accumulation",
 }
 chunks = {"lat": 10, "lon": 10, "time": -1}
-
-
-def load_cmip_dictionary(
-    activity_ids=["CMIP", "ScenarioMIP"],
-    experiment_ids=["historical", "ssp370"],  # , "ssp126", "ssp245",  "ssp585"
-    member_ids=["r1i1p1f1"],
-    source_ids=["MIROC6"],  # BCC-CSM2-MR"]
-    table_ids=["day"],
-    grid_labels=["gn"],
-    variable_ids=["tasmax"],
-):
-    """Loads CMIP6 GCM dataset dictionary based on input criteria.
-
-    Parameters
-    ----------
-    activity_ids : list, optional
-        [activity_ids in CMIP6 catalog], by default ["CMIP", "ScenarioMIP"],
-    experiment_ids : list, optional
-        [experiment_ids in CMIP6 catalog], by default ["historical", "ssp370"],  ex:#  "ssp126", "ssp245",  "ssp585"
-    member_ids : list, optional
-        [member_ids in CMIP6 catalog], by default ["r1i1p1f1"]
-    source_ids : list, optional
-        [source_ids in CMIP6 catalog], by default ["MIROC6"]
-    table_ids : list, optional
-        [table_ids in CMIP6 catalog], by default ["day"]
-    grid_labels : list, optional
-        [grid_labels in CMIP6 catalog], by default ["gn"]
-    variable_ids : list, optional
-        [variable_ids in CMIP6 catalog], by default ['tasmax']
-
-    Returns
-    -------
-    [dictionary]
-        [dictionary containing available xarray datasets]
-    """
-    col_url = "https://cmip6downscaling.blob.core.windows.net/cmip6/pangeo-cmip6.json"
-    print("intake")
-    full_subset = intake.open_esm_datastore(col_url).search(
-        activity_id=activity_ids,
-        experiment_id=experiment_ids,
-        member_id=member_ids,
-        source_id=source_ids,
-        table_id=table_ids,
-        grid_label=grid_labels,
-        variable_id=variable_ids,
-    )
-    print("to dictionary")
-    ds_dict = full_subset.to_dataset_dict(
-        zarr_kwargs={"consolidated": True, "decode_times": True, "use_cftime": True},
-        storage_options={
-            "account_name": "cmip6downscaling",
-            "account_key": os.environ.get("AccountKey", None),
-        },
-        progressbar=False,
-    )
-    print("return dict")
-    return ds_dict
-
-
-# # tests
-
-
-def get_store(bucket, prefix, account_key=None):
-    """helper function to create a zarr store"""
-
-    if account_key is None:
-        account_key = os.environ.get("AccountKey", None)
-
-    store = zarr.storage.ABSStore(
-        bucket, prefix=prefix, account_name="cmip6downscaling", account_key=account_key
-    )
-    return store
-
-
-def open_era5(var):
-    print("getting stores")
-    col = intake.open_esm_datastore(
-        "https://cmip6downscaling.blob.core.windows.net/cmip6/ERA5_catalog.json"
-    )
-    stores = col.df.zstore
-    era5_var = variable_name_dict[var]
-    store_list = stores[stores.str.contains(era5_var)].to_list()
-    # store_list[:10]
-    ds = xr.open_mfdataset(
-        store_list,
-        engine="zarr",  # these options set the inputs and how to read them
-        consolidated=True,
-        parallel=True,  # these options speed up the reading of individual datasets (before they are combined)
-        combine="by_coords",  # these options tell xarray how to combine the data
-        # data_vars=['air_temperature_at_2_metres_1hour_Maximum']  # these options limit the amount of data that is read to only variables of interest
-    ).drop("time1_bounds")
-    print("return mfdataset")
-    return ds
-
-
-def load_obs(obs_id, variable, time_period, domain):
-    """
-
-    Parameters
-    ----------
-    obs_id : [type]
-        [description]
-    variable : [type]
-        [description]
-    time_period : [type]
-        [description]
-    domain : [type]
-        [description]
-
-    Returns
-    -------
-    [xarray dataset]
-        [Chunked {time:-1,lat=10,lon=10}]
-    """
-    ## most of this can be deleted once new ERA5 dataset complete
-    if obs_id == "ERA5":
-        print("open era5")
-        full_obs = open_era5(variable)
-        print("resample era5")
-        obs = (
-            full_obs[variable_name_dict[variable]]
-            # .sel(time=time_period, lon=domain["lon"], lat=domain["lat"])
-            .sel(time=time_period)
-            # .resample(time="1d")
-            # .mean()
-            .rename(variable)
-            # .load(scheduler="threads")  # GOAL! REMOVE THE `LOAD`!
-        )
-        obs = obs.sel(time=obs.time.dt.hour==12)
-        obs = obs.sel(time=obs.time.dt.day==1)
-
-        # obs = obs.resample(time="1D").max()
-    print(obs)
-    return obs
-
-
-###### ALL OF THIS IS ICING ON THE CAKE- NOT NECESSARY NOW
-# @task
-# def setuprun()
-#     '''
-#     based upon input gcms/scenarios determine which tasks you need to complete
-#     then the remaining tasks will loop through (anticipates a not-dense set of gcms/scenarios/dsms)
-#     - shared across downscaling methods
-#     '''
-
-#     return experiment_ids
-
-# def get_grid(dataset):
-#     if dataset=='ERA5':
-#         return '25km'
-#     elif dataset=='CMIP.MIROC.MIROC6.historical.day.gn':
-#         return 'not25km'
-
-# def check_preparation(experiment):
-#     # what grid are you on
-#     # does the weights file for that grid exist
-#     # does the coarse obs for that grid exist
-#     # do the spatial anomolies for those coarse obs (time period matters) exist
-
-
-@task(checkpoint=True)
-def get_weight_file(grid_name_gcm, grid_name_obs):
-    happy = "yeah"
-    return happy
-
-
-# @task(checkpoint=True)
-def get_coarse_obs(
-    obs,
-    gcm_ds_single_time_slice,
-):
-    """[summary]
-
-    Parameters
-    ----------
-    obs : xarray dataset
-        chunked in space (lat=-1, lon=-1, time=1)
-    gcm_ds_single_time_slice : [type]
-        Chunked in space
-
-    Returns
-    -------
-    [type]
-        [Chunked in space (lat=-1, lon=-1, time=1)]
-    """
-    # TEST TODO: Check that obs is chunked appropriately and throw error if not
-    # Like: assert obs.chunks == (lat=-1, lon=-1, time=1) - then eventually we can move the rechunker in as an `else`
-    regridder = xe.Regridder(obs, gcm_ds_single_time_slice, "bilinear")
-
-    obs_coarse = regridder(obs)
-    # then write it out
-    # # obs_coarse.to_zarr()
-    print(obs_coarse.chunks)
-    return obs_coarse
-
-
-def get_spatial_anomolies(coarse_obs, fine_obs):
-    # check if this has been done, if do the math
-    # if it has been done, just read them in
-    """[summary]
-
-    Parameters
-    ----------
-    coarse_obs : [type]
-        [chunked in space (lat=-1,lon=-1,time=1)]
-    fine_obs : [type]
-        [chunked in space (lat=-1,lon=-1,time=1)]
-
-    Returns
-    -------
-    [type]
-        [description]
-    """
-    # check chunks specs & run regridder if needed
-    regridder = xe.Regridder(
-        coarse_obs, fine_obs.isel(time=0), "bilinear", extrap_method="nearest_s2d"
-    )
-
-    obs_interpolated = regridder(coarse_obs)
-    spatial_anomolies = obs_interpolated - fine_obs
-    seasonal_cycle_spatial_anomolies = spatial_anomolies.groupby("time.month").mean()
-    print(seasonal_cycle_spatial_anomolies.chunks)
-    return seasonal_cycle_spatial_anomolies
-
-
-@task(log_stdout=True)
-def print_x(x):
-    print(x)
 
 
 @task(  # target="{flow_name}.txt", checkpoint=True,
@@ -378,7 +108,7 @@ def preprocess_bcsd(
     )
     if rerun:
         print("load obs")
-        obs_ds = load_obs(
+        obs_da = load_obs(
             obs_id,
             variable,
             time_period=slice(train_period_start, train_period_end),
@@ -398,10 +128,12 @@ def preprocess_bcsd(
         gcm_ds_single_time_slice = gcm_ds.isel(
             time=0
         )  # .load() #TODO: check whether we need the load here
+        chunks_dict_obs_maps = calc_auspicious_chunks_dict(obs_ds, chunk_dims=('time',)) # you'll need to put comma after the one element tuple
+
         rechunked_obs, rechunked_obs_path = rechunk_dataset(
-            obs_ds,
-            # obs_ds.to_dataset(name=variable), # Might have to revert this..
-            chunks_dict={"tasmax": (1, -1, -1)},
+            # obs_ds,
+            obs_da.to_dataset(name=variable), # Might have to revert this..
+            chunks_dict={"tasmax": chunks_dict_obs_maps},
             connection_string=connection_string,
             max_mem="1GB",
         )
@@ -415,44 +147,19 @@ def preprocess_bcsd(
         spatial_anomolies = get_spatial_anomolies(coarse_obs, rechunked_obs)
 
         # save the coarse obs because it might be used by another gcm
-        coarse_obs.to_dataset(name=variable).to_zarr(
+        coarse_obs.to_zarr(
             coarse_obs_store, mode="w", consolidated=True
         )
 
-        spatial_anomolies.to_dataset(name=variable).to_zarr(
+        spatial_anomolies.to_zarr(
             spatial_anomolies_store, mode="w", consolidated=True
         )
         return coarse_obs
 
 
-# @task
-# def biascorrect(X, y, train_period, predict_period, model):
-#     '''
-#     fit the model at coarse gcm scale and then predict!
-# fit
-# predict
-# return y_hat
-#     '''
-
-# @task
-# def postprocess(y_hat, spatial_anomolies):
-#     '''
-#     Interpolate, add back in the spatial anomolies from the coarsening, and write to store
-#     '''
-#     y_hat.
-# write out downscaled bias-corrected
-def gcm_munge(ds):
-    if ds.lat[0] < ds.lat[-1]:
-        ds = ds.reindex({"lat": ds.lat[::-1]})
-    ds = ds.drop(["lat_bnds", "lon_bnds", "time_bnds", "height", "member_id"]).squeeze(
-        drop=True
-    )
-    return ds
-
-
 @task(log_stdout=True, nout=3)
 def prep_bcsd_inputs(
-    # coarse_obs,
+    coarse_obs,
     gcm,
     obs_id,
     train_period_start,
@@ -491,16 +198,16 @@ def prep_bcsd_inputs(
     xarray datasets
         Chunked in lat=10,lon=10,time=-1
     """
-    # X = load_cmip_dictionary()["CMIP.MIROC.MIROC6.historical.day.gn"]
-    # X = gcm_munge(X)
-    # X = X.sel(**domain, time=slice(train_period_start, train_period_end))
-    # # coarse_obs_store = fsspec.get_mapper(
-    # #     f"az://cmip6/intermediates/{obs_id}_{gcm[0]}_{train_period_start}_{train_period_end}_{variable}.zarr",
-    # #     connection_string=connection_string,
-    # # )
-    # y = coarse_obs.sel(**domain)
-    # # finally set the gcm time index to be the same as the obs one (and the era5 index is datetime64 which sklearn prefers)
-    # X["time"] = y.time.values
+    X = load_cmip_dictionary()["CMIP.MIROC.MIROC6.historical.day.gn"]
+    X = gcm_munge(X)
+    X = X.sel(**domain, time=slice(train_period_start, train_period_end))
+    # coarse_obs_store = fsspec.get_mapper(
+    #     f"az://cmip6/intermediates/{obs_id}_{gcm[0]}_{train_period_start}_{train_period_end}_{variable}.zarr",
+    #     connection_string=connection_string,
+    # )
+    y = coarse_obs.sel(**domain)
+    # finally set the gcm time index to be the same as the obs one (and the era5 index is datetime64 which sklearn prefers)
+    X["time"] = y.time.values
     chunks_dict = {"tasmax": {"time": -1, "lat": 10, "lon": 10}}
     X_rechunked, X_rechunked_path = rechunk_dataset(
         X, chunks_dict=chunks_dict, connection_string=connection_string, max_mem="1GB"
@@ -660,41 +367,30 @@ with Flow(name=flow_name) as flow:
         rerun=True,
     )  # can remove this once we have caching working
 
-    # X, y, X_predict = prep_bcsd_inputs(
-    #     # coarse_obs,
-    #     gcm,
-    #     obs_id=obs,
-    #     train_period_start=train_period_start,
-    #     train_period_end=train_period_end,
-    #     predict_period_start=predict_period_start,
-    #     predict_period_end=predict_period_end,
-    #     variable=variable,
-    #     out_bucket="cmip6",
-    #     domain=domain,
-    # )
+    X, y, X_predict = prep_bcsd_inputs(
+        coarse_obs, # adding coarse_obs is a hack to force make prep_bcsd_inputs dependent on preprocess. could also
+        # just add the dependency to the flow description
+        gcm,
+        obs_id=obs,
+        train_period_start=train_period_start,
+        train_period_end=train_period_end,
+        predict_period_start=predict_period_start,
+        predict_period_end=predict_period_end,
+        variable=variable,
+        out_bucket="cmip6",
+        domain=domain,
+    )
 
-    # y_predict = fit_and_predict(X, y, X_predict)
-    # postprocess_bcsd(
-    #     y_predict,
-    #     gcm,
-    #     obs,
-    #     train_period_start,
-    #     train_period_end,
-    #     variable,
-    #     predict_period_start,
-    #     predict_period_end,
-    # )
+    y_predict = fit_and_predict(X, y, X_predict)
+    postprocess_bcsd(
+        y_predict,
+        gcm,
+        obs,
+        train_period_start,
+        train_period_end,
+        variable,
+        predict_period_start,
+        predict_period_end,
+    )
 # flow.visualize()
 flow.run(parameters=run_hyperparameters)
-
-
-# with Flow(name=flow_name) as flow:
-#     ds_dict = test_intake()
-#     print(ds_dict)
-
-
-# for run_hyperparameters in list_of_hyperparameter_dicts:
-#     flow.run(parameters=run_hyperparameters)
-# make all permutations of list_of_hyperparameter_dicts:
-
-# task.map(list_of_hyperparameter_dicts)
--- cmip6_downscaling/workflows/utils.py
+++ cmip6_downscaling/workflows/utils.py
@@ -27,7 +27,14 @@ def temp_file_name():
     letters = string.ascii_lowercase
     return "".join(random.choice(letters) for i in range(10))
 
-
+def delete_chunks_encoding(ds):
+    for data_var in ds.data_vars:
+        if 'chunks' in ds[data_var].encoding:
+            del ds[data_var].encoding['chunks']
+    for coord in ds.coords:
+        if 'chunks' in ds[coords].encoding:
+            del ds[coords].encoding['chunks']
+            
 def rechunk_dataset(ds, chunks_dict, connection_string, max_mem="500MB"):
     """[summary]
 
@@ -58,9 +65,9 @@ def rechunk_dataset(ds, chunks_dict, connection_string, max_mem="500MB"):
     store_tgt = fsspec.get_mapper(
         "az://cmip6/temp/{}.zarr".format(path_tgt), connection_string=connection_string
     )
-
-    if "chunks" in ds["tasmax"].encoding:
-        del ds["tasmax"].encoding["chunks"]
+    print(path_tmp)
+    print(path_tgt)
+    delete_chunks_encoding(ds)
 
     api.rechunk(
         ds,
@@ -69,9 +76,31 @@ def rechunk_dataset(ds, chunks_dict, connection_string, max_mem="500MB"):
         target_store=store_tgt,
         temp_store=store_tmp,
     ).execute()
-    print("done with rechunk")
-    rechunked_ds = xr.open_zarr(store_tgt)  # ideally we want consolidated=True but
-    # it isn't working for some reason
-    print("done with open_zarr")
-    print(rechunked_ds["tasmax"].data.chunks)
+    rechunked_ds = xr.open_zarr(store_tgt)  # ideally we want consolidated=True but it seems that functionality isn't offered in rechunker right now
     return rechunked_ds, path_tgt
+
+
+def calc_auspicious_chunks_dict(ds,
+                    target_size='100mb', 
+                     chunk_dims=('lat', 'lon')):
+    assert target_size=='100mb',"Apologies, but not implemented for anything but 100m right now!"
+    assert type(chunk_dims)==tuple,"Your chunk_dims likely includes one string but needs a comma after it! to be a tuple!"
+    target_size_bytes=100e6
+    array_dims = dict(zip(ds.dims, ds.shape))
+    chunks_dict = {}
+    # dims not in chunk_dims should be one chunk (length -1)
+    for dim in array_dims.keys():
+        if dim not in chunk_dims:
+            chunks_dict[dim] = -1
+    # calculate the bytesize given the dtype
+    data_bytesize = int(re.findall(r'\d+', str(ds.dtype))[0])
+    # calculate single non_chunked_size based upon dtype
+    smallest_size_one_chunk = data_bytesize * np.prod([array_dims[dim] for dim in chunks_dict.keys()])
+    # the dims in chunk_dims should be of a square size that creates ~100 mb
+    perfect_chunk = target_size_bytes/smallest_size_one_chunk
+    # then make reasonable chunk size by rounding up (avoids corner case of it rounding down to 0...)
+    perfect_chunk_length = int(np.ceil(perfect_chunk ** (1/len(chunk_dims))))
+    for dim in chunk_dims:
+        chunks_dict[dim] = perfect_chunk_length
+    
+    return chunks_dict
