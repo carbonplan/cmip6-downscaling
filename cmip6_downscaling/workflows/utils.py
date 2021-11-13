ad69a3743f98a32ed1567f3ccf084b6bfa2c85ce
tcchiao <tcchiao@gmail.com>
2021-11-13 04:12:04 +0000

    batches

--- cmip6_downscaling/workflows/utils.py
+++ cmip6_downscaling/workflows/utils.py
@@ -16,3 +16,33 @@ def get_store(prefix, account_key=None):
         account_key=account_key,
     )
     return store
+
+
+def generate_batches(n, batch_size, buffer_size, one_indexed=False):
+    """
+    ds must have a dimension called time that is a valid datetime index 
+    """
+    # TODO: add tests. if buffer_size == 0, batches == cores 
+    # construct 2 test cases 
+    
+    cores = []
+    batches = []
+    if one_indexed:
+        xmin = 1
+        xmax = n + 1
+    else:
+        xmin = 0
+        xmax = n
+    for start in range(xmin, xmax, batch_size):
+        end = min(start + batch_size, xmax)
+        cores.append(np.arange(start, end))
+        
+        # add buffer 
+        end = end + buffer_size
+        start = start - buffer_size
+        batch = np.arange(start, end)
+        batch[batch < xmin] += n
+        batch[batch > xmax - 1] -= n
+        batches.append(batch)
+        
+    return batches, cores
