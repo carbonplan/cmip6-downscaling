9cb6af0afbf075de7c183017eea8dd99f58a0cf3
Oriana Chegwidden <oriana@carbonplan.org>
2022-01-11 01:47:42 +0000

    making summary steps prefect tasks

--- cmip6_downscaling/analysis/analysis.py
+++ cmip6_downscaling/analysis/analysis.py
@@ -48,3 +48,49 @@ def annual_summary(ds):
             print(f'{var} not implemented')
 
     return out_ds
+
+def run_analysis_notebook(gcm,
+    scenario,
+    train_period_start,
+    train_period_end,
+    predict_period_start,
+    predict_period_end,
+    variable,
+    latmin, 
+    latmax,
+    lonmin, 
+    lonmax):
+    """Create a jupyter notebook with analyses about the run with 
+    parameters specified here.
+
+    Parameters
+    ----------
+    gcm : str
+        Input GCM
+    scenario: str
+        Input GCM scenario
+    train_period_start : str
+        Date for training period start (e.g. '1985')
+    train_period_end : str
+        Date for training period end (e.g. '2015')
+    predict_period_start : str
+        Date for prediction period start (e.g. '2090')
+    predict_period_end : str
+        Date for prediction period end (e.g. '2090')
+    variable: str
+        The variable included in the dataset.
+    latmin : str
+        Minimum latitude in domain
+    latmax : str
+        Maximum latitude in domain
+    lonmin : str
+        Minimum longitude in domain
+    lonmax : str
+        Maximum longitude in domain
+    """
+    run_id = f'{gcm}-{scenario}-{train_period_start}-{train_period_end}-{predict_period_start}-{predict_period_end}-{latmin}-{latmax}-{lonmin}-{lonmax}-{variable}.zarr'
+    pm.execute_notebook(
+        '/home/jovyan/cmip6-downscaling/notebooks/analyses.ipynb',
+        f'/home/jovyan/cmip6-downscaling/notebooks/output/analyses_{run_id}.ipynb',
+        parameters={'run_id': run_id}
+        )
