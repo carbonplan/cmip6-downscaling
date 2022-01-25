f86aa23c366c63e6226fb39931f21692a7ee7253
Joseph Hamman <jhamman@ucar.edu>
2022-01-25 09:56:06 -0800

    fix test cloud workflow

--- cmip6_downscaling/workflows/test_cloud_flow.py
+++ cmip6_downscaling/workflows/test_cloud_flow.py
@@ -1,50 +1,14 @@
-import os
 import time
 import uuid
 
 import ESMF
 import numpy as np
 from dask.distributed import get_worker
-from dask_kubernetes import KubeCluster, make_pod_spec
 from prefect import Flow, task
-from prefect.executors import DaskExecutor
-from prefect.run_configs import KubernetesRun
-from prefect.storage import Azure
 
-image = "carbonplan/cmip6-downscaling-prefect:2021.12.06"
-storage = Azure("prefect")
+from cmip6_downscaling.runtimes import get_runtime
 
-env = {
-    'AZURE_STORAGE_CONNECTION_STRING': os.environ['AZURE_STORAGE_CONNECTION_STRING'],
-    'OMP_NUM_THREADS': '1',
-    'MPI_NUM_THREADS': '1',
-    'MKL_NUM_THREADS': '1',
-    'OPENBLAS_NUM_THREADS': '1',
-    'DASK_DISTRIBUTED__WORKER__RESOURCES__TASKSLOTS': '1',
-}
-
-run_config = KubernetesRun(
-    cpu_request=4,
-    memory_request="8Gi",
-    image=image,
-    labels=["az-eu-west"],
-    env=env,
-)
-pod_spec = make_pod_spec(
-    image=image,
-    memory_limit="4Gi",
-    memory_request="4Gi",
-    threads_per_worker=2,
-    cpu_limit=2,
-    cpu_request=2,
-    env=env,
-)
-pod_spec.spec.containers[0].args.extend(['--resources', 'TASKSLOTS=1'])
-
-executor = DaskExecutor(
-    cluster_class=lambda: KubeCluster(pod_spec, deploy_mode='remote'),
-    adapt_kwargs={"minimum": 2, "maximum": 2},
-)
+runtime = get_runtime()
 
 
 @task(log_stdout=True, tags=['dask-resource:TASKSLOTS=1'])
@@ -67,7 +31,10 @@ def make_grid(shape):
 
 
 with Flow(
-    name="test_cloud_flow", storage=storage, run_config=run_config, executor=executor
+    name="test_cloud_flow",
+    storage=get_runtime.storage,
+    run_config=get_runtime.run_config,
+    executor=get_runtime.executor,
 ) as flow:
 
     # nums = range(4)
