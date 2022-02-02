aee78ccf1539bf7957b6fc7e3db760348b5f077e
norland r hagen <norlandrhagen@gmail.com>
2022-02-02 23:44:53 +0000

    lint

--- cmip6_downscaling/__init__.py
+++ cmip6_downscaling/__init__.py
@@ -4,6 +4,6 @@ from donfig import Config
 from cmip6_downscaling.config import _defaults
 
 config = Config("cmip6_downscaling", defaults=[_defaults])
-config.config_lock = SerializableLock
+config.config_lock = SerializableLock()
 
 CLIMATE_NORMAL_PERIOD = (1970, 2000)
