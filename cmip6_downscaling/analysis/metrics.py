477724310f71c0d1b2f85713a2869c9d24f08c6f
Oriana Chegwidden <oriana@carbonplan.org>
2022-02-03 17:17:08 +0000

    fix build

--- cmip6_downscaling/analysis/metrics.py
+++ cmip6_downscaling/analysis/metrics.py
@@ -1,57 +1,10 @@
 import numpy as np
 import xarray as xr
-from esda.moran import Moran
 
 
 def weighted_mean(ds, *args, **kwargs):
     weights = ds.time.dt.days_in_month
     return ds.weighted(weights).mean(dim='time')
-
-
-def time_mean(obj):
-    obj = obj.resample(time='AS').map(weighted_mean, dim='time')
-    return obj.mean('time')
-
-
-def seasonal_cycle_mean(obj):
-    return obj.mean(('x', 'y'))
-
-
-def seasonal_cycle_std(obj):
-    return obj.mean(('x', 'y'))
-
-
-def moransI(obj, weights=None):
-    """
-    Inputs:
-    obj: xarray object for which you want the autocorrelation
-    weights: same shape as obj, but with weights which will be applied to
-    obj to scale the autocorrelation. Simplest could be all ones.
-    """
-    if not weights:
-        weights = np.ones_like(obj.values)
-
-    return Moran(obj.values, weights)
-
-
-def calc(obj, compute=False):
-    """
-    This function takes an object and then calculates a
-    series of metrics for that object. It returns it
-    as a dictionary object of xarray objects. If the compute flag
-    is turned on these objects will be in memory. Otherwise the
-    computations will be lazy.
-    """
-
-    metrics = {}
-    if compute:
-        metrics['time_mean'] = time_mean(obj).load()
-        metrics['seasonal_cycle_mean'] = seasonal_cycle_mean(obj).load()
-    else:
-        metrics['time_mean'] = time_mean(obj)
-        metrics['seasonal_cycle_mean'] = seasonal_cycle_mean(obj)
-
-    return metrics
 
 
 def days_temperature_threshold(
