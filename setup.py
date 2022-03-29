2624f5f5ba9edf0872731f46fdf7bf6e41b15fc1
Anderson Banihirwe <axbanihirwe@ualr.edu>
2022-03-29 13:49:34 -0600

    Add pyupgrade and prettier hooks

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@
 
 """The setup script."""
 
+
+import pathlib
 from setuptools import find_packages, setup
 
 with open("requirements.txt") as f:
     INSTALL_REQUIRES = f.read().strip().split("\n")
 
-with open("README.md") as f:
-    LONG_DESCRIPTION = f.read()
-
+LONG_DESCRIPTION = pathlib.Path("README.md").read_text()
 PYTHON_REQUIRES = ">=3.7"
 
 description = "climate downscaling using cmip6 data"
@@ -30,5 +30,4 @@ setup(
     license="MIT",
     keywords="carbon, data, climate",
     use_scm_version={"version_scheme": "post-release", "local_scheme": "dirty-tag"},
-    setup_requires=["setuptools_scm", "setuptools>=30.3.0"],
 )
