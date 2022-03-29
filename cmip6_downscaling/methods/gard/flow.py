e0741d43e6ea345d236a3bc019fa54d50fb568b9
Oriana Chegwidden <oriana@carbonplan.org>
2022-03-29 22:44:23 +0000

    initial commit of gard flow and tasks

---
+++ cmip6_downscaling/methods/gard/flow.py
@@ -0,0 +1,128 @@
+import warnings
+
+from prefect import Flow, Parameter
+
+from cmip6_downscaling import runtimes
+from cmip6_downscaling.methods.common.tasks import (  # annual_summary,; monthly_summary,; pyramid,; run_analyses,
+    get_experiment,
+    get_obs,
+    make_run_parameters,
+    rechunk,
+    regrid,
+)
+from cmip6_downscaling.methods.gard.tasks import coarsen_and_interpolate
+
+warnings.filterwarnings(
+    "ignore",
+    "(.*) filesystem path not explicitly implemented. falling back to default implementation. This filesystem may not be tested",
+    category=UserWarning,
+)
+
+runtime = runtimes.get_runtime()
+print(runtime)
+
+
+with Flow(
+    name="gard", storage=runtime.storage, run_config=runtime.run_config, executor=runtime.executor
+) as flow:
+
+    run_parameters = make_run_parameters(
+        method=Parameter("method"),
+        obs=Parameter("obs"),
+        model=Parameter("model"),
+        member=Parameter("member"),
+        grid_label=Parameter("grid_label"),
+        table_id=Parameter("table_id"),
+        scenario=Parameter("scenario"),
+        variable=Parameter("variable"),
+        latmin=Parameter("latmin"),
+        latmax=Parameter("latmax"),
+        lonmin=Parameter("lonmin"),
+        lonmax=Parameter("lonmax"),
+        train_dates=Parameter("train_period"),
+        predict_dates=Parameter("predict_period"),
+    )
+
+    # input datasets
+    obs_path = get_obs(run_parameters)
+    obs_full_space_path = rechunk(path=obs_path, chunking_pattern='full_space')
+    experiment_train_path = get_experiment(run_parameters, time_subset='train_period')
+    experiment_predict_path = get_experiment(run_parameters, time_subset='predict_period')
+
+    # after regridding coarse_obs will have smaller array size in space but still
+    # be chunked finely along time. but that's good to get it for regridding back to
+    # the interpolated obs in next task
+    # interpolated obs should have same exact chunking schema as ds at `obs_full_space_path`
+    # TODO: WRITE THIS
+    interpolated_obs_full_space_path = coarsen_and_interpolate(
+        obs_full_space_path, experiment_train_path
+    )
+
+    # TODO: do we need the templates as well for the rechunking? probably defer to bcsd flow here
+    interpolated_obs_full_time_path = rechunk(
+        interpolated_obs_full_space_path, template=experiment_train_path, pattern='full_time'
+    )
+
+    # get gcm data into full space to prep for interpolation
+    experiment_train_full_space_path = rechunk(experiment_train_path, chunking_pattern="full_space")
+    experiment_predict_full_space_path = rechunk(
+        experiment_predict_path, chunking_pattern="full_space"
+    )
+
+    # interpolate gcm to finescale
+    experiment_train_fine_full_space_path = regrid(
+        source_path=experiment_train_full_space_path, target_grid_path=obs_path
+    )
+    experiment_predict_fine_full_space_path = regrid(
+        source_path=experiment_predict_full_space_path, target_grid_path=obs_path
+    )
+    # TODO: do we need the templates as well for the rechunking? probably defer to bcsd flow here
+    experiment_train_fine_full_time_path = rechunk(
+        experiment_train_fine_full_space_path, chunking_pattern="full_time"
+    )
+    experiment_predict_fine_full_time_path = rechunk(
+        experiment_predict_fine_full_space_path,
+        chunking_pattern="full_time",
+        chunking_template_file=interpolated_obs_full_time_path,
+    )
+
+    # TODO: WRITE THIS
+    # fit and predict (TODO: put the transformation steps currently in the prep_gard_input task into the fit and predict step)
+    # model_output_path = fit_and_predict(
+    #     xtrain=interpolated_obs_full_time_path,
+    #     ytrain=experiment_train_fine_full_time_path,
+    #     xpred=experiment_predict_fine_full_time_path,
+    #     run_parameters=run_parameters,
+    # )  # [transformation, gard_model_options])
+
+    # # TODO: WRITE THIS
+    # # post process (bring in scrf into the postprocess task)
+    # # necessary input chunk pattern:
+    # final_bcsd_full_time_path = postprocess_gard(
+    #     model_output_path, spatial_anomalies_path, run_parameters
+    # )  # fine-scale maps (full_space) (time: 365)
+
+    # # temporary aggregations - these come out in full time
+    # monthly_summary_path = monthly_summary(final_bcsd_full_time_path, run_parameters)
+    # annual_summary_path = annual_summary(final_bcsd_full_time_path, run_parameters)
+
+    # # analysis notebook (shared with BCSD)
+    # analysis_location = run_analyses(final_bcsd_full_time_path, run_parameters)
+
+    # # since pyramids require full space we now rechunk everything into full
+    # # space before passing into pyramid step. we probably want to add a cleanup
+    # # to this step in particular since otherwise we will have an exact
+    # # duplicate of the daily, monthly, and annual datasets
+    # final_bcsd_full_space_path = rechunk(final_bcsd_full_time_path, chunking_pattern='full_space')
+
+    # # make temporal summaries
+    # monthly_summary_full_space_path = rechunk(monthly_summary_path, chunking_pattern='full_space')
+    # annual_summary_full_space_path = rechunk(annual_summary_path, chunking_pattern='full_space')
+
+    # # pyramids
+    # daily_pyramid_path = pyramid(final_bcsd_full_space_path, levels=4)
+    # monthly_pyramid_path = pyramid(monthly_summary_full_space_path, levels=4)
+    # annual_pyramid_path = pyramid(annual_summary_full_space_path, levels=4)
+
+    # # if config.get('run_options.cleanup_flag') is True:
+    # #     cleanup.run_rsfip(gcm_identifier, obs_identifier)
---
+++ cmip6_downscaling/methods/gard/tasks.py
@@ -0,0 +1,63 @@
+import xarray as xr
+import xesmf as xe
+from carbonplan_data.metadata import get_cf_global_attrs
+from prefect import task
+from upath import UPath
+
+from ... import config
+from ..._version import __version__
+from ..common.utils import zmetadata_exists
+
+version = __version__
+scratch_dir = UPath(config.get("storage.scratch.uri"))
+intermediate_dir = UPath(config.get("storage.intermediate.uri")) / __version__
+results_dir = UPath(config.get("storage.results.uri")) / __version__
+use_cache = config.get('run_options.use_cache')
+
+
+@task(tags=['dask-resource:TASKSLOTS=1'], log_stdout=True)
+def coarsen_and_interpolate(fine_path: UPath, coarse_path: UPath) -> UPath:
+    """
+    Coarsen up obs and then interpolate it back to the original finescale grid.
+    Parameters
+    ----------
+    fine_path : UPath
+        Path to finescale (likely observational) dataset
+    coarse_path : UPath
+        Path to coarse scale that will be the template for the coarsening.
+
+    Returns
+    -------
+    UPath
+        Path to interpolated dataset.
+    """
+    ds_name = f'source_path_{fine_path.name}/target_path_{coarse_path.name}'
+    target = intermediate_dir / 'coarsen_and_interpolate' / ds_name
+
+    if use_cache and zmetadata_exists(target):
+        print(f'found existing target: {target}')
+        return target
+
+    fine_ds = xr.open_zarr(fine_path)
+    target_ds = xr.open_zarr(coarse_path)
+
+    # coarsen
+    regridder = xe.Regridder(fine_ds, target_ds, "bilinear", extrap_method="nearest_s2d")
+    coarse_ds = regridder(fine_ds)
+
+    # interpolate back to the fine grid
+    regridder = xe.Regridder(coarse_ds, fine_ds, "bilinear", extrap_method="nearest_s2d")
+    interpolated_ds = regridder(coarse_ds)
+
+    interpolated_ds.attrs.update({'title': ds_name}, **get_cf_global_attrs(version=version))
+    interpolated_ds.to_zarr(target, mode='w')
+
+    return target
+
+
+# @task(tags=['dask-resource:TASKSLOTS=1'], log_stdout=True)
+# def fit_and_predict(
+#         xtrain: UPath,
+#         ytrain: UPath,
+#         xpred: UPath,
+#         run_parameters: RunParameters) -> UPath:
