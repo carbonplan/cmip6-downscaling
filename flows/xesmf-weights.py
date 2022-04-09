2651af7165a4e2c92039aba70d00384bef5c96f8
Anderson Banihirwe <axbanihirwe@ualr.edu>
2022-04-09 04:59:58 +0000

    update flow

--- flows/xesmf-weights.py
+++ flows/xesmf-weights.py
@@ -1,15 +1,28 @@
 from __future__ import annotations
 
+import dask
 from prefect import Flow, task, unmapped
+from prefect.tasks.control_flow import merge
+from upath import UPath
 
 from cmip6_downscaling import config
 from cmip6_downscaling.runtimes import get_runtime
+
+dask.config.set(
+    {
+        'distributed.dashboard.link': 'https://prod.azure.carbonplan.2i2c.cloud/{JUPYTERHUB_SERVICE_PREFIX}/proxy/{port}/status'
+    }
+)
 
 config.set(
     {
         'runtime.cloud.extra_pip_packages': 'git+https://github.com/carbonplan/cmip6-downscaling.git@main git+https://github.com/intake/intake-esm.git git+https://github.com/carbonplan/ndpyramid tabulate'
     }
 )
+
+folder = 'xesmf-weights'
+
+scratch_dir = UPath(config.get('storage.scratch.uri')) / folder
 
 runtime = get_runtime()
 
@@ -26,7 +39,6 @@ def get_stores() -> list[dict]:
         .first()
         .reset_index()
         .drop(columns=['member_id', 'dcpp_init_year', 'version', 'activity_id', 'institution_id'])
-        .sample(n=5)
     ).to_dict(orient='records')
 
     print(len(stores))
@@ -40,46 +52,76 @@ def generate_weights_pyramid(ds_in, levels):
     from ndpyramid.regrid import make_grid_ds
 
     weights_pyramid = datatree.DataTree()
-    for level in range(levels):
-        ds_out = make_grid_ds(level=level)
-        regridder = xe.Regridder(ds_in, ds_out, method='bilinear')
-        w = regridder.weights.data
-        dim = 'n_s'
-        ds = xr.Dataset(
-            {
-                'S': (dim, w.data),
-                'col': (dim, w.coords[1, :] + 1),
-                'row': (dim, w.coords[0, :] + 1),
-            }
-        )
-        weights_pyramid[str(level)] = ds
-    weights_pyramid.ds.attrs['levels'] = levels
-    weights_pyramid.ds.attrs['regrid_method'] = regridder.method
+    with dask.config.set(scheduler='sync'):
+        for level in range(levels):
+            ds_out = make_grid_ds(level=level)
+            regridder = xe.Regridder(ds_in, ds_out, method='bilinear')
+            w = regridder.weights.data
+            dim = 'n_s'
+            ds = xr.Dataset(
+                {
+                    'S': (dim, w.data),
+                    'col': (dim, w.coords[1, :] + 1),
+                    'row': (dim, w.coords[0, :] + 1),
+                }
+            )
+            weights_pyramid[str(level)] = ds
+
+        weights_pyramid.ds.attrs['levels'] = levels
+        weights_pyramid.ds.attrs['regrid_method'] = regridder.method
 
     return weights_pyramid
 
 
 @task(log_stdout=True)
-def generate_weights(store: dict, levels: int) -> bool:
+def generate_weights(store: dict, levels: int) -> dict:
 
     import xarray as xr
+
+    method = 'bilinear'
+
+    target = (
+        scratch_dir
+        / store['source_id']
+        / store['table_id']
+        / store['grid_label']
+        / f'{method}_{levels}.zarr'
+    )
+
+    print(f'weights pyramid path: {target}')
 
     try:
         ds_in = xr.open_dataset(store['zstore'], engine='zarr', chunks={})
         weights_pyramid = generate_weights_pyramid(ds_in, levels)
-        print(weights_pyramid.ds.attrs)
-        print(store)
 
-    except Exception:
+        weights_pyramid.to_zarr(target, mode='w')
+
+    except Exception as e:
         print(f'Failed to load {store["zstore"]}')
-    return True
+        print(e)
+    attrs = {
+        'source_id': store['source_id'],
+        'table_id': store['table_id'],
+        'grid_label': store['grid_label'],
+        'regrid_method': method,
+        'levels': levels,
+        'path': str(target),
+    }
+    return attrs
 
 
-with Flow(
-    name='xesmf-weights',
-    storage=runtime.storage,
-    run_config=runtime.run_config,
-    executor=runtime.executor,
-) as flow:
+@task(log_stdout=True)
+def catalog(vals):
+    import pandas as pd
+
+    target = scratch_dir / 'weights.csv'
+    df = pd.DataFrame(vals)
+    df.to_csv(target, index=False)
+    print(target)
+
+
+with Flow(name='xesmf-weights', storage=runtime.storage, run_config=runtime.run_config) as flow:
     stores = get_stores()
-    _ = generate_weights.map(stores, levels=unmapped(2))
+    attrs = generate_weights.map(stores, levels=unmapped(2))
+    vals = merge(attrs)
+    _ = catalog(vals)
