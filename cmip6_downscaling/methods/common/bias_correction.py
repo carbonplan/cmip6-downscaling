57ababc16fe265d7196f88b5082e9d359193977c
Raphael Hagen <norlandrhagen@gmail.com>
2022-05-12 12:18:08 -0700

    Update cmip6_downscaling/methods/common/bias_correction.py
    Co-authored-by: Joe Hamman <joe@carbonplan.org>

--- cmip6_downscaling/methods/common/bias_correction.py
+++ cmip6_downscaling/methods/common/bias_correction.py
@@ -84,7 +84,6 @@ def bias_correct_gcm_by_method(
         sc.fit(gcm_hist)
         return sc.transform(gcm_pred)
     elif method == 'quantile_mapper':
-
         qm = PointWiseDownscaler(model=QuantileMapper(detrend=True), dim='time')
         qm.fit(obs)
         return qm.transform(gcm_pred)
