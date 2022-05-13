562dba5cd3a8783532b5cc358f7aa1f26711c8fb
Anderson Banihirwe <axbanihirwe@ualr.edu>
2022-05-13 07:24:35 -0600

    fix page references/links

--- docs/components/contents.js
+++ docs/components/contents.js
@@ -1,7 +1,7 @@
 export const contents = {
   'Getting Started': ['Overview', 'Quick start'],
   Tutorials: ['Intro to Statistical Downscaling'],
-  'How-to Guides': ['Running Prefect Flows'],
+  'How-to Guides': ['Running Flows'],
   Explanation: ['Input Datasets', 'Downscaling Methods'],
   Reference: ['API'],
 }
