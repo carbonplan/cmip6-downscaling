b9883f6a094cfbbd120d7d2ea416a9f96898354a
Anderson Banihirwe <axbanihirwe@ualr.edu>
2022-05-13 06:38:11 -0600

    Add placeholder pages

--- docs/components/contents.js
+++ docs/components/contents.js
@@ -1,5 +1,7 @@
 export const contents = {
   'Getting Started': ['Overview', 'Quick start'],
+  Tutorials: ['Intro to Statistical Downscaling'],
   'How-to Guides': ['Running Prefect Flows'],
+  Explanation: ['Input Datasets', 'Downscaling Methods'],
   Reference: ['API'],
 }
