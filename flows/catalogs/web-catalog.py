9a53c7722318ede0a9aae542726c74135a77023f
Anderson Banihirwe <axbanihirwe@ualr.edu>
2022-05-12 23:46:40 -0600

    update flow

--- flows/catalogs/web-catalog.py
+++ flows/catalogs/web-catalog.py
@@ -1,3 +1,6 @@
+import json
+
+import fsspec
 from prefect import Flow, Parameter, task
 
 from cmip6_downscaling import config, runtimes
@@ -98,15 +101,106 @@ def get_cmip6_pyramids(paths: list[str]):
     return builder.df.to_dict(orient='records')
 
 
+def parse_cmip6_downscaled_pyramid(data):
+    import intake
+    from upath import UPath
+
+    parameters = data['parameters']
+
+    datasets = data['datasets']
+
+    query = {
+        'source_id': parameters['model'],
+        'member_id': parameters['member'],
+        'experiment_id': parameters['scenario'],
+        'variable_id': parameters['variable'],
+    }
+
+    cat_url = "https://cmip6downscaling.blob.core.windows.net/cmip6/pangeo-cmip6.json"
+    cat = intake.open_esm_datastore(cat_url).search(table_id='day', **query)
+    records = cat.df.to_dict(orient='records')
+    if records and len(records) == 1:
+        entry = records[0]
+        query['institution_id'] = entry['institution_id']
+        query['activity_id'] = entry['activity_id']
+        query['original_datasets_uris'] = [
+            f"https://cmip6downscaling.azureedge.net/{str(entry['zstore']).split('//')[-1]}"
+        ]
+
+    template = {**query, 'method': parameters['method']}
+    results = []
+    if 'daily_pyramid_path' in datasets:
+        daily_pyramid_attrs = {
+            **template,
+            **{
+                'timescale': 'day',
+                'uri': f"https://cmip6downscaling.azureedge.net/{str(datasets['daily_pyramid_path']).split('//')[-1]}",
+                'name': UPath(datasets['daily_pyramid_path']).name,
+            },
+        }
+        results.append(daily_pyramid_attrs)
+    if 'monthly_pyramid_path' in datasets:
+        monthly_pyramid_attrs = {
+            **template,
+            **{
+                'timescale': 'month',
+                'uri': f"https://cmip6downscaling.azureedge.net/{str(datasets['monthly_pyramid_path']).split('//')[-1]}",
+                'name': UPath(datasets['monthly_pyramid_path']).name,
+            },
+        }
+        results.append(monthly_pyramid_attrs)
+    if 'annual_pyramid_path' in datasets:
+        yearly_pyramid_attrs = {
+            **template,
+            **{
+                'timescale': 'year',
+                'uri': f"https://cmip6downscaling.azureedge.net/{str(datasets['annual_pyramid_path']).split('//')[-1]}",
+                'name': UPath(datasets['annual_pyramid_path']).name,
+            },
+        }
+        results.append(yearly_pyramid_attrs)
+
+    for attrs in results:
+        if attrs['timescale'] in {'year', 'month'}:
+            if attrs['variable_id'] in {'tasmin', 'tasmax'}:
+                attrs['aggregation'] = 'mean'
+            elif attrs['variable_id'] in {'pr'}:
+                attrs['aggregation'] = 'sum'
+    return results
+
+
 @task(log_stdout=True)
-def create_catalog(*, catalog_path: str, cmip6_pyramids=None, era5_pyramids=None):
+def get_cmip6_downscaled_pyramids(path):
+
+    mapper = fsspec.get_mapper(path)
+    fs = mapper.fs
+    latest_runs = fs.glob(path)
+    datasets = []
+    for run in latest_runs:
+        with fs.open(run) as f:
+            data = json.load(f)
+
+        datasets += parse_cmip6_downscaled_pyramid(data)
+    return datasets
+
+
+@task(log_stdout=True)
+def create_catalog(
+    *,
+    catalog_path: str,
+    cmip6_raw_pyramids=None,
+    cmip6_downscaled_pyramids=None,
+    era5_pyramids=None,
+):
     import json
 
     import fsspec
 
     datasets = []
-    if cmip6_pyramids:
-        datasets += cmip6_pyramids
+    if cmip6_raw_pyramids:
+        datasets += cmip6_raw_pyramids
+    if cmip6_downscaled_pyramids:
+        datasets += cmip6_downscaled_pyramids
     if era5_pyramids:
         datasets += era5_pyramids
 
@@ -134,5 +228,14 @@ with Flow(
         'web-catalog-path',
         default='az://flow-outputs/results/pyramids/combined-cmip6-era5-pyramids-catalog-web.json',
     )
-    cmip6_pyramids = get_cmip6_pyramids(paths)
-    create_catalog(catalog_path=web_catalog_path, cmip6_pyramids=cmip6_pyramids)
+    downscaled_pyramids = Parameter(
+        'downscaled-pyramids-path',
+        default='az://flow-outputs/results/0.0.post563+dirty/runs/**/latest.json',
+    )
+    cmip6_raw_pyramids = get_cmip6_pyramids(paths)
+    cmip6_downscaled_pyramids = get_cmip6_downscaled_pyramids(downscaled_pyramids)
+    create_catalog(
+        catalog_path=web_catalog_path,
+        cmip6_raw_pyramids=cmip6_raw_pyramids,
+        cmip6_downscaled_pyramids=cmip6_downscaled_pyramids,
+    )
