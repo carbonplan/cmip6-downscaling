37ae39a58992daca20e78242860d49e72a48258d
Joseph Hamman <jhamman@ucar.edu>
2022-05-13 12:30:15 -0700

    more updates to maca flow

--- cmip6_downscaling/methods/maca/flow.py
+++ cmip6_downscaling/methods/maca/flow.py
@@ -1,4 +1,3 @@
-
 from prefect import Flow, Parameter
 
 from cmip6_downscaling import runtimes
@@ -14,7 +13,13 @@ from cmip6_downscaling.methods.common.tasks import (  # run_analyses,
     regrid,
     time_summary,
 )
-from cmip6_downscaling.methods.maca.tasks import calc_epoch_trend, bias_correction
+from cmip6_downscaling.methods.maca.tasks import (
+    bias_correction,
+    combine_regions,
+    construct_analogs,
+    epoch_trend,
+    split_by_region,
+)
 
 runtime = runtimes.get_runtime()
 print(runtime)
@@ -43,15 +48,14 @@ with Flow(
         train_dates=Parameter("train_period"),
         predict_dates=Parameter("predict_period"),
     )
-    
+
     p = {}
 
     ## Step 0: tasks to get inputs and set up
     ## Step 1: Common Grid -- this step is skipped since it seems like an unnecessary extra step for convenience
 
-
     # input datasets
-    p['gcm_to_obs_weights'] = get_weights(run_parameters=run_parameters, direction='gcm_to_obs')
+    # p['gcm_to_obs_weights'] = get_weights(run_parameters=run_parameters, direction='gcm_to_obs')
     p['obs_to_gcm_weights'] = get_weights(run_parameters=run_parameters, direction='obs_to_gcm')
 
     # get original resolution observations
@@ -71,26 +75,33 @@ with Flow(
     # TODO: in order to properly do a 31 year average, might need to run this step with the entire future period in GCMs
     # but this might be too memory intensive in the later task
     # BIG JOB
-    p['coarse_epoch_trend_path'], p['detrended_data_path'] = calc_epoch_trend(p['experiment_train_path'], run_parameters)
+    p['coarse_epoch_trend_path'], p['detrended_data_path'] = epoch_trend(
+        p['experiment_train_path'], run_parameters
+    )
 
     # get gcm
     # 1981-2100 extent time subset
-    p['experiment_predict_full_time_path'] = rechunk(p['experiment_predict_path'], pattern='full_time')
+    p['experiment_predict_full_time_path'] = rechunk(
+        p['experiment_predict_path'], pattern='full_time'
+    )
 
     ## Step 3: Coarse Bias Correction
     # rechunk to make detrended data match the coarse obs
     # TODO: check whether we want full-time AND template (or just template)
-    p['detrend_gcm_rechunk_coarse_obs'] = rechunk(p['experiment_predict_path'], template=p['coarse_obs_path'])
+    p['detrend_gcm_rechunk_coarse_obs_path'] = rechunk(
+        p['experiment_predict_path'], template=p['coarse_obs_path']
+    )
 
-        # inputs should be in full-time
+    # inputs should be in full-time
     p['bias_corrected_gcm_path'] = bias_correction(
         gcm_path=p['detrended_data_path'],
         obs_path=p['coarse_obs_path'],
-        run_parameters=run_parameters)
-    
+        run_parameters=run_parameters,
+    )
+
     # do epoch adjustment again for multiplicative variables, see MACA v1 vs. v2 guide for details
     # if label in ['pr', 'huss', 'vas', 'uas']:
-    #     coarse_epoch_trend_2 = calc_epoch_trend_task(
+    #     coarse_epoch_trend_2 = epoch_trend_task(
     #         data=bias_corrected_gcm,
     #         train_period_start=train_period_start,
     #         train_period_end=train_period_end,
@@ -111,24 +122,26 @@ with Flow(
     # rechunk into full space and cache the output
     p['bc_gcm_full_space'] = rechunk(p['bc_gcm'], pattern='full_space')
 
-    # subset into regions
-    # subdomains_list, subdomains_dict, mask, n_subdomains = get_subdomains_task(
-    #     ds_obs=ds_obs_full_space
-    # )
-
     # everything should be rechunked to full space and then subset
     p['coarse_obs_path'] = regrid(
         p['obs_full_space_path'], p['experiment_train_path'], weights_path=p['obs_to_gcm_weights']
     )
 
-    p['region_paths'] = split_by_region.map(region_ids)  # region_ids is a list of integers or keys that map to the region mask ids
-    p['constructed_analogs'] = construct_analogs.map(p['region_paths']) # i'm not sure there is more than one step here
+    p['bc_gcm_region_paths'] = split_by_region(p['bc_gcm_full_space_path'])
+    p['coarse_obs_region_paths'] = split_by_region(p['coarse_obs_path'])
+    p['fine_obs_region_paths'] = split_by_region(p['fine_obs_path'])
 
-    # can we put all of these into one task?
-    p['full_grid_detrended_path'] = combine_regions(p['constructed_analogs'])
-    p['full_grid_path'] = maca_reapply_trend(p['full_grid_detrended_path'])
-    p['final_full_grid_path'] = bias_correct_final( p['full_grid_path'])
+    p['constructed_analogs_path'] = construct_analogs.map(
+        p['bc_gcm_region_paths'], p['coarse_obs_region_paths'], p['fine_obs_region_paths']
+    )
 
+    p['full_grid_detrended_path'] = combine_regions(
+        p['constructed_analogs_path'], p['fine_obs_path']
+    )
+
+    # combine these two?
+    # p['full_grid_path'] = maca_reapply_trend(p['full_grid_detrended_path'])
+    # p['final_full_grid_path'] = bias_correct_final( p['full_grid_path'])
 
     # all inputs into the map function needs to be a list
     # ds_gcm_list, ds_obs_coarse_list, ds_obs_fine_list = subset_task(
@@ -176,9 +189,9 @@ with Flow(
     #     year_rolling_window=epoch_adjustment_year_rolling_window,
     #     gcm_identifier=gcm_identifier,
     # )
-    p['obs_full_time_path'] = rechunk(path=p['obs_path'], pattern="full_time")
 
     ## Step 6: Fine Bias Correction
+    p['final_maca_full_time_path'] = bias_correction()
 
     # final_output = maca_fine_bias_correction_task(
     #     ds_gcm=epoch_replaced_gcm,
@@ -190,3 +203,36 @@ with Flow(
     #     buffer_size=bias_correction_buffer_size,
     #     gcm_identifier=gcm_identifier,
     # )
+
+    # temporary aggregations - these come out in full time
+    p['monthly_summary_path'] = time_summary(p['final_maca_full_time_path'], freq='1MS')
+    p['annual_summary_path'] = time_summary(p['final_maca_full_time_path'], freq='1AS')
+
+    # analysis notebook
+    # analysis_location = run_analyses(p['final_maca_full_time_path'], run_parameters)
+
+    # since pyramids require full space we now rechunk everything into full
+    # space before passing into pyramid step. we probably want to add a cleanup
+    # to this step in particular since otherwise we will have an exact
+    # duplicate of the daily, monthly, and annual datasets
+    p['final_maca_full_space_path'] = rechunk(p['final_maca_full_time_path'], pattern='full_space')
+
+    # make temporal summaries
+    p['monthly_summary_full_space_path'] = rechunk(p['monthly_summary_path'], pattern='full_space')
+    p['annual_summary_full_space_path'] = rechunk(p['annual_summary_path'], pattern='full_space')
+
+    # pyramids
+    p['pyramid_weights'] = get_pyramid_weights(run_parameters=run_parameters, levels=4)
+
+    p['daily_pyramid_path'] = pyramid(
+        p['final_maca_full_space_path'], weights_pyramid_path=p['pyramid_weights'], levels=4
+    )
+    p['monthly_pyramid_path'] = pyramid(
+        p['monthly_summary_full_space_path'], weights_pyramid_path=p['pyramid_weights'], levels=4
+    )
+    p['annual_pyramid_path'] = pyramid(
+        p['annual_summary_full_space_path'], weights_pyramid_path=p['pyramid_weights'], levels=4
+    )
+
+    # finalize
+    finalize(p, run_parameters)
--- cmip6_downscaling/methods/maca/utils.py
+++ cmip6_downscaling/methods/maca/utils.py
@@ -1,13 +1,109 @@
+from __future__ import annotations
+
 import numpy as np
+import pandas as pd
+import xarray as xr
 
 
-def generate_batches(n, batch_size, buffer_size, one_indexed=False):
+def add_circular_temporal_pad(data: xr.Dataset, offset: int, timeunit: str = 'D') -> xr.Dataset:
+    """
+    Pad the beginning of data with the last values of data, and pad the end of data with the first values of data.
+
+    Parameters
+    ----------
+    data : xr.Dataset
+        data to be padded, must have time as a dimension
+    offset : int
+        number of time points to be padded
+    timeunit : str
+        unit of offset. Default is 'D' = days
+
+    Returns
+    -------
+    padded: xr.Dataset
+        The padded dataset
+    """
+
+    padded = data.pad(time=offset, mode='wrap')
+
+    time_coord = padded.time.values
+    time_coord[:offset] = (data.time[:offset] - pd.Timedelta(offset, timeunit)).values
+    time_coord[-offset:] = (data.time[-offset:] + pd.Timedelta(offset, timeunit)).values
+
+    padded = padded.assign_coords({'time': time_coord})
+
+    return padded
+
+
+def days_in_year(year: int, use_leap_year: bool = False) -> int:
+    """
+    Returns the number of days in the year input.
+
+    Parameters
+    ----------
+    year : int
+        year in question
+    use_leap_year : bool
+        whether to return 366 for leap years
+
+    Returns
+    -------
+    n_days_in_year : int
+        Number of days in the year of question
+    """
+    if (year % 4 == 0) and use_leap_year:
+        return 366
+    return 365
+
+
+def pad_with_edge_year(data: xr.Dataset) -> xr.Dataset:
+    """
+    Pad data with the first and last year available. similar to the behavior of np.pad(mode='edge')
+    but uses the 365 edge values instead of repeating only 1 edge value (366 if leap year)
+
+    Parameters
+    ----------
+    data : xr.Dataset
+        data to be padded, must have time as a dimension
+
+    Returns
+    -------
+    padded : xr.Dataset
+        The padded dataset
+    """
+
+    def pad_with(vector, pad_width, iaxis, kwargs):
+        pstart = pad_width[0]
+        pend = pad_width[1]
+        if pstart > 0:
+            start = vector[pstart : pstart * 2]
+            vector[:pstart] = start
+        if pend > 0:
+            end = vector[-pend * 2 : -pend]
+            vector[-pend:] = end
+
+    # TODO: change use_leap_year to True once we unify the GCMs into Gregorian calendars
+    prev_year = days_in_year(data.time[0].dt.year.values - 1, use_leap_year=False)
+    next_year = days_in_year(data.time[-1].dt.year.values + 1, use_leap_year=False)
+    padded = data.pad({'time': (prev_year, next_year)}, mode=pad_with)
+    time_coord = padded.time.values
+    time_coord[:prev_year] = (data.time[:prev_year] - pd.Timedelta(prev_year, 'D')).values
+    time_coord[-next_year:] = (data.time[-next_year:] + pd.Timedelta(next_year, 'D')).values
+
+    padded = padded.assign_coords({'time': time_coord})
+
+    return padded
+
+
+def generate_batches(
+    n: int, batch_size: int, buffer_size: int, one_indexed: bool = False
+) -> tuple[list, list]:
     """
     Given the max value n, batch_size, and buffer_size, returns batches (include the buffer) and
     cores (exclude the buffer). For the smallest numbers, the largest values would be included in the buffer, and
     vice versa. For example, with n=10, batch_size=5, buffer_size=3, one_indexed=False. The `cores` output will contain
     [[0, 1, 2, 3, 4], [5, 6, 7, 8, 9]], and `batches` output will contain [[7, 8, 9, 0, 1, 2, 3, 4, 5, 6, 7], [2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2]].
-    
+
     Parameters
     ----------
     n : int
@@ -18,8 +114,8 @@ def generate_batches(n, batch_size, buffer_size, one_indexed=False):
         The number of buffer values to include in each batch in both directions.
     one_indexed : bool
         Whether we should consider n to be one indexed or not. With n = 2, one_indexed=False would generate cores containing [0, 1].
-        One_indexed=True would generate cores containing [1, 2].
-    
+        ``one_indexed=True`` would generate cores containing [1, 2].
+
     Returns
     -------
     batches : List
