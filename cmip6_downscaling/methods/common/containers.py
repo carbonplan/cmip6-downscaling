9a417ca27c0689657dbe76da20f1564a57e339cd
Anderson Banihirwe <axbanihirwe@ualr.edu>
2022-05-17 16:44:34 -0600

    fix imports

--- cmip6_downscaling/methods/common/containers.py
+++ cmip6_downscaling/methods/common/containers.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 
 from dataclasses import dataclass
 
-from .utils import str_to_hash
+from ...utils import str_to_hash
 
 
 @dataclass
--- cmip6_downscaling/methods/common/tasks.py
+++ cmip6_downscaling/methods/common/tasks.py
@@ -25,10 +25,10 @@ from upath import UPath
 from xarray_schema import DataArraySchema, DatasetSchema
 from xarray_schema.base import SchemaError
 
+from ... import __version__ as version, config
 from ...data.cmip import get_gcm
 from ...data.observations import open_era5
 from ...utils import str_to_hash
-from . import __version__ as version, config
 from .containers import RunParameters
 from .utils import calc_auspicious_chunks_dict, resample_wrapper, subset_dataset, zmetadata_exists
 
