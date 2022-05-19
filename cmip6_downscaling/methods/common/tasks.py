3172b9cea167cbd1e0c778674f9fdb4b21123fbb
Joe Hamman <joe@carbonplan.org>
2022-05-19 23:02:09 +0000

    back to dask

--- cmip6_downscaling/methods/common/tasks.py
+++ cmip6_downscaling/methods/common/tasks.py
@@ -275,7 +275,7 @@ def rechunk(
         temp_store=temp_store,
         target_options={k: {'compressor': zarr.Blosc(clevel=1)} for k in chunks_dict},
         temp_options={k: {'compressor': None} for k in chunks_dict},
-        executor='prefect',
+        executor='dask',
     )
 
     rechunk_plan.execute()
