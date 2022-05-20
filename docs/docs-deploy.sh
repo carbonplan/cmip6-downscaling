119b89a0a3cc7e4f51463705c4c3e6a9ec557450
Kata Martin <katasm@gmail.com>
2022-05-19 17:55:02 -0700

    Revert "Test script"
    This reverts commit 4d9aed0cdf5f7120d4846ca9ef0fe8ecc332984f.

--- docs/docs-deploy.sh
+++ docs/docs-deploy.sh
@@ -1,16 +1,12 @@
 #!/bin/bash
 
-echo "VERCEL_ENV: $VERCEL_ENV"
-echo "VERCEL_GIT_COMMIT_MESSAGE: $VERCEL_GIT_COMMIT_MESSAGE"
-
-
 if [[ "$VERCEL_ENV" == production || "$VERCEL_GIT_COMMIT_MESSAGE" == *[docs]* ]] ; then
   # Proceed with the build
-	echo "✅ - Build would have proceeded"
-  exit 0;
+	echo "✅ - Build can proceed"
+  exit 1;
 
 else
   # Don't build
-  echo "🛑 - Build would have been cancelled"
+  echo "🛑 - Build cancelled"
   exit 0;
 fi
