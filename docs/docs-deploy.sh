4d9aed0cdf5f7120d4846ca9ef0fe8ecc332984f
Kata Martin <katasm@gmail.com>
2022-05-19 17:50:59 -0700

    Test script

--- docs/docs-deploy.sh
+++ docs/docs-deploy.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
+echo "VERCEL_ENV: $VERCEL_ENV"
+echo "VERCEL_GIT_COMMIT_MESSAGE: $VERCEL_GIT_COMMIT_MESSAGE"
+
+
 if [[ "$VERCEL_ENV" == production || "$VERCEL_GIT_COMMIT_MESSAGE" == *[docs]* ]] ; then
   # Proceed with the build
-	echo "✅ - Build can proceed"
-  exit 1;
+	echo "✅ - Build would have proceeded"
+  exit 0;
 
 else
   # Don't build
-  echo "🛑 - Build cancelled"
+  echo "🛑 - Build would have been cancelled"
   exit 0;
 fi
