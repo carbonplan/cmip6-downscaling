8f4b8997e32d71cb24bf6b7b89970b9f6117c1c1
norland r hagen <norlandrhagen@gmail.com>
2022-06-27 21:43:03 +0000

    switched order of postprocess and added method = nearest to .sel

--- cmip6_downscaling/data/cmip.py
+++ cmip6_downscaling/data/cmip.py
@@ -45,7 +45,6 @@ def postprocess(ds: xr.Dataset, to_standard_calendar: bool = True) -> xr.Dataset
         # drop height variable
         if 'height' in ds:
             ds = ds.drop('height')
-
         # squeeze length 1 dimensions
         ds = ds.squeeze(drop=True)
 
@@ -121,17 +120,18 @@ def load_cmip(
             grid_label=grid_labels,
             variable_id=variable_ids,
         )
-
         keys = list(col_subset.keys())
         if len(keys) != 1:
             raise ValueError(f'intake-esm search returned {len(keys)}, expected exactly 1.')
         ds = col_subset[keys[0]]().to_dask()
+
         if time_slice:
             ds = ds.sel(time=time_slice)
-        ds = ds.pipe(postprocess)
+
         if 'plev' in ds.coords:
             # select the 500 mb level for winds
-            ds = ds.sel(plev=50000.0).drop('plev')
+            ds = ds.sel(plev=50000.0, method='nearest').drop('plev')
+        ds = ds.pipe(postprocess)
 
         # convert to mm/day - helpful to prevent rounding errors from very tiny numbers
         if 'pr' in ds:
