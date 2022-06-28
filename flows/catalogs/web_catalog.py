25872406a15b03dc773be0a9b58eaf88361d3e10
Anderson Banihirwe <axbanihirwe@ualr.edu>
2022-06-28 16:15:31 -0600

    add license information to the web catalog

--- flows/catalogs/web_catalog.py
+++ flows/catalogs/web_catalog.py
@@ -1,8 +1,12 @@
+import datetime
 import functools
 import json
+import traceback
 
 import fsspec
+import intake
 from prefect import Flow, Parameter, task
+from upath import UPath
 
 from cmip6_downscaling import config, runtimes
 
@@ -12,15 +16,94 @@ config.set(
     }
 )
 
+
+def get_license(source_id: str) -> dict:
+
+    LICENSE_MAPPING = {
+        'ACCESS-CM2': '',
+        'ACCESS-ESM1-5': 'CC-BY-4.0',
+        'AWI-CM-1-1-MR': 'CC-BY-4.0',
+        'AWI-ESM-1-1-LR': 'CC-BY-4.0',
+        'BCC-CSM2-MR': 'CC-BY-SA-4.0',
+        'BCC-ESM1': 'CC-BY-SA-4.0',
+        'CAMS-CSM1-0': 'CC-BY-SA-4.0',
+        'CAS-ESM2-0': '',
+        'CESM2': 'CC-BY-4.0',
+        'CESM2-FV2': 'CC-BY-4.0',
+        'CESM2-WACCM': 'CC-BY-4.0',
+        'CESM2-WACCM-FV2': 'CC-BY-4.0',
+        'CMCC-CM2-HR4': 'CC-BY-4.0',
+        'CMCC-CM2-SR5': 'CC-BY-4.0',
+        'CMCC-ESM2': 'CC-BY-4.0',
+        'CNRM-CM6-1': 'CC-BY-4.0',
+        'CNRM-CM6-1-HR': 'CC-BY-4.0',
+        'CNRM-ESM2-1': 'CC-BY-4.0',
+        'CanESM5': 'CC-BY-SA-4.0',
+        'CanESM5-CanOE': 'CC-BY-SA-4.0',
+        'EC-Earth3': 'CC-BY-4.0',
+        'EC-Earth3-AerChem': 'CC-BY-4.0',
+        'EC-Earth3-CC': 'CC-BY-4.0',
+        'EC-Earth3-Veg': 'CC-BY-4.0',
+        'EC-Earth3-Veg-LR': 'CC-BY-4.0',
+        'FGOALS-g3': 'CC-BY-SA-4.0',
+        'FIO-ESM-2-0': 'CC-BY-SA-4.0',
+        'GFDL-CM4': 'CC-BY-4.0',
+        'GISS-E2-1-G': 'CC0-1.0',
+        'GISS-E2-1-G-CC': 'CC0-1.0',
+        'GISS-E2-1-H': 'CC0-1.0',
+        'HadGEM3-GC31-LL': 'CC-BY-4.0',
+        'HadGEM3-GC31-MM': 'CC-BY-4.0',
+        'ICON-ESM-LR': 'CC-BY-4.0',
+        'IITM-ESM': 'CC-BY-SA-4.0',
+        'INM-CM4-8': 'CC-BY-SA-4.0',
+        'INM-CM5-0': 'CC-BY-SA-4.0',
+        'IPSL-CM6A-LR': 'CC-BY-4.0',
+        'KACE-1-0-G': 'CC-BY-SA-4.0',
+        'MCM-UA-1-0': '',
+        'MIROC-ES2H': 'CC-BY-4.0',
+        'MIROC-ES2L': 'CC-BY-4.0',
+        'MIROC6': 'CC-BY-4.0',
+        'MPI-ESM-1-2-HAM': 'CC-BY-SA-4.0',
+        'MPI-ESM1-2-HR': 'CC-BY-SA-4.0',
+        'MPI-ESM1-2-LR': 'CC-BY-SA-4.0',
+        'MRI-ESM2-0': 'CC-BY-SA-4.0',
+        'NESM3': 'CC-BY-SA-4.0',
+        'NorCPM1': 'CC-BY-4.0',
+        'NorESM2-LM': 'CC-BY-4.0',
+        'NorESM2-MM': 'CC-BY-4.0',
+        'SAM0-UNICON': 'CC-BY-SA-4.0',
+        'TaiESM1': 'CC-BY-4.0',
+        'UKESM1-0-LL': 'CC-BY-4.0',
+    }
+
+    license = LICENSE_MAPPING.get(source_id)
+    if license:
+        licences = {
+            'CC-BY-4.0': {
+                'name': 'CC-BY-4.0',
+                'url': 'https://creativecommons.org/licenses/by/4.0/',
+            },
+            'CC-BY-SA-4.0': {
+                'name': 'CC-BY-SA-4.0',
+                'url': 'https://creativecommons.org/licenses/by-sa/4.0/',
+            },
+            'CC0-1.0': {
+                'name': 'CC0-1.0',
+                'url': 'https://creativecommons.org/publicdomain/zero/1.0/',
+            },
+        }
+
+        return licences[license]
+    else:
+        return {}
+
+
 runtime = runtimes.CloudRuntime()
 
 
-def parse_cmip6(store, cdn):
-    import traceback
+def parse_cmip6(store: str, root_path: str) -> dict[str, str]:
 
-    import intake
     from ecgtools.builder import INVALID_ASSET, TRACEBACK
-    from upath import UPath
 
     from cmip6_downscaling.methods.common.utils import zmetadata_exists
 
@@ -63,12 +146,12 @@ def parse_cmip6(store, cdn):
             elif variable_id in {'pr'}:
                 aggregation = 'sum'
 
-        uri = f"{cdn}/{str(path).split('//')[-1]}"
+        uri = f"{root_path}/{str(path).split('//')[-1]}"
         cat_url = "https://cmip6downscaling.blob.core.windows.net/cmip6/pangeo-cmip6.json"
         cat = intake.open_esm_datastore(cat_url)
         stores = cat.search(**query).df.zstore.tolist()
         if stores:
-            original_dataset_uris = [f"{cdn}/{dataset.split('//')[-1]}" for dataset in stores]
+            original_dataset_uris = [f"{root_path}/{dataset.split('//')[-1]}" for dataset in stores]
         else:
             original_dataset_uris = []
         return {
@@ -79,6 +162,7 @@ def parse_cmip6(store, cdn):
             'uri': uri,
             'original_dataset_uris': original_dataset_uris,
             'method': 'raw',
+            'license': get_license(source_id),
         }
 
     except Exception:
@@ -86,7 +170,7 @@ def parse_cmip6(store, cdn):
 
 
 @task(log_stdout=True)
-def get_cmip6_pyramids(paths: list[str], cdn: str):
+def get_cmip6_pyramids(paths: list[str], cdn: str, root_path: str) -> list[dict]:
     import ecgtools
 
     builder = ecgtools.Builder(
@@ -95,17 +179,14 @@ def get_cmip6_pyramids(paths: list[str], cdn: str):
         joblib_parallel_kwargs={'n_jobs': -1, 'verbose': 1},
         exclude_patterns=["*.json"],
     )
-    builder.build(parsing_func=functools.partial(parse_cmip6, cdn=cdn))
+    builder.build(parsing_func=functools.partial(parse_cmip6, root_path=root_path))
 
     return builder.df.to_dict(orient='records')
 
 
-def parse_cmip6_downscaled_pyramid(data, cdn: str):
-    import intake
-    from upath import UPath
+def parse_cmip6_downscaled_pyramid(data, cdn: str, root_path: str) -> list[dict]:
 
     parameters = data['parameters']
-
     datasets = data['datasets']
 
     query = {
@@ -122,9 +203,10 @@ def parse_cmip6_downscaled_pyramid(data, cdn: str):
         entry = records[0]
         query['institution_id'] = entry['institution_id']
         query['activity_id'] = entry['activity_id']
-        query['original_dataset_uris'] = [f"{cdn}/{str(entry['zstore']).split('//')[-1]}"]
+        query['original_dataset_uris'] = [f"{root_path}/{str(entry['zstore']).split('//')[-1]}"]
+        query['license'] = get_license(query['source_id'])
 
-    template = {**query, 'method': parameters['method']}
+    template = {**query, 'method': parameters['method'], 'license': ''}
     results = []
     if 'daily_pyramid_path' in datasets:
         daily_pyramid_attrs = {
@@ -167,7 +249,7 @@ def parse_cmip6_downscaled_pyramid(data, cdn: str):
 
 
 @task(log_stdout=True)
-def get_cmip6_downscaled_pyramids(path, cdn):
+def get_cmip6_downscaled_pyramids(path, cdn: str, root_path: str):
 
     mapper = fsspec.get_mapper(path)
     fs = mapper.fs
@@ -181,7 +263,7 @@ def get_cmip6_downscaled_pyramids(path, cdn):
             data['datasets'].get(arg) is not None
             for arg in ['daily_pyramid_path', 'monthly_pyramid_path', 'annual_pyramid_path']
         ):
-            datasets += parse_cmip6_downscaled_pyramid(data, cdn)
+            datasets += parse_cmip6_downscaled_pyramid(data, cdn, root_path)
     return datasets
 
 
@@ -193,10 +275,6 @@ def create_catalog(
     cmip6_downscaled_pyramids=None,
     era5_pyramids=None,
 ):
-    import datetime
-    import json
-
-    import fsspec
 
     datasets = []
     if cmip6_raw_pyramids:
@@ -211,7 +289,7 @@ def create_catalog(
         "title": "CMIP6 downscaling catalog",
         "description": "",
         "history": "",
-        "last_updated": datetime.datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S"),
+        "last_updated": datetime.datetime.now(datetime.timezone.utc).strftime("%Y-%m-%d %H:%M:%S"),
         "datasets": datasets,
     }
 
@@ -239,8 +317,10 @@ with Flow(
     # https://cmip6downscaling.azureedge.net
     cdn = Parameter('cdn', default='https://cmip6downscaling.blob.core.windows.net')
 
-    cmip6_raw_pyramids = get_cmip6_pyramids(paths, cdn)
-    cmip6_downscaled_pyramids = get_cmip6_downscaled_pyramids(downscaled_pyramids, cdn)
+    root_path = Parameter('root-path', 'https://cmip6downscaling.blob.core.windows.net')
+
+    cmip6_raw_pyramids = get_cmip6_pyramids(paths, cdn, root_path)
+    cmip6_downscaled_pyramids = get_cmip6_downscaled_pyramids(downscaled_pyramids, cdn, root_path)
     create_catalog(
         catalog_path=web_catalog_path,
         cmip6_raw_pyramids=cmip6_raw_pyramids,
