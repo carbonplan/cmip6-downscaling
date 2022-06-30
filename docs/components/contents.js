5f725c62a4cb8fc6138ab68a266d47a6b1054f59
Max Jones <meghanj@alum.mit.edu>
2022-06-30 20:32:29 +0000

    Remove intro to statistical downscaling page

--- docs/components/contents.js
+++ docs/components/contents.js
@@ -1,6 +1,5 @@
 export const contents = {
   'Getting Started': ['Overview', 'Quick start'],
-  Tutorials: ['Intro to Statistical Downscaling'],
   'How-to Guides': ['Running Flows'],
   Explanation: ['Input Datasets', 'Downscaling Methods'],
   Reference: ['API'],
--- docs/pages/cmip6-downscaling/intro-to-statistical-downscaling.md
+++
@@ -1,5 +0,0 @@
-import Section from '../../components/section'
-
-# Intro to Statistical Downscaling
-
-export default ({ children }) => <Section name='Intro to Statistical Downscaling'>{children}</Section>
