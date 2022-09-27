552ef6c423e46cf4968cfe92ce792caff3945b89
Max Jones <meghanj@alum.mit.edu>
2022-09-26 21:39:30 -0400

    Add unit test for open_era5

---
+++ tests/test_data.py
@@ -0,0 +1,12 @@
+import numpy as np
+from xarray_schema import DataArraySchema, DatasetSchema
+
+from cmip6_downscaling.data.observations import open_era5
+
+
+def test_open_era5():
+    ds = open_era5('tasmin', slice('2020', '2020'))
+    schema = DatasetSchema(
+        {'tasmin': DataArraySchema(dtype=np.floating, name='tasmin', dims=['time', 'lat', 'lon'])}
+    )
+    schema.validate(ds)
