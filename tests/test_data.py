4c04c8b2c5ea93d09e3ed5b122f7911f587f57fc
Max Jones <meghanj@alum.mit.edu>
2022-09-26 21:57:36 -0400

    Add wind var

--- tests/test_data.py
+++ tests/test_data.py
@@ -5,8 +5,9 @@ from cmip6_downscaling.data.observations import open_era5
 
 
 def test_open_era5():
-    ds = open_era5('tasmin', slice('2020', '2020'))
+    ds = open_era5(['tasmin', 'ua'], slice('2020', '2020'))
     schema = DatasetSchema(
-        {'tasmin': DataArraySchema(dtype=np.floating, name='tasmin', dims=['time', 'lat', 'lon'])}
+        {'tasmin': DataArraySchema(dtype=np.floating, name='tasmin', dims=['time', 'lat', 'lon'])},
+        {'ua': DataArraySchema(dtype=np.floating, name='ua', dims=['time', 'lat', 'lon'])},
     )
     schema.validate(ds)
